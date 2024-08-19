83fdfe1615e714d99a66806caabde196d0a4003a
Lautaro Lombardo <lautilomba517@gmail.com>
2024-08-18 23:05:31 -0300

    inicializacion

---
+++ index.js
@@ -0,0 +1,17 @@
+//
+var cors = require("cors");
+var express = require("express");
+var app = express();
+
+const port = 8000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", function (req, res) {
+    res.send("Hola mundo");
+});
+
+app.listen(port);
+
+console.log(`Servicio web corriendo en http://localhost:${port}/`);
