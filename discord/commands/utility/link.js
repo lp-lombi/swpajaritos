2a073961e3830bbf2b98984cac010ccdb033cfa8
Lautaro Lombardo <lautilomba517@gmail.com>
2024-09-20 14:52:02 -0300

    update link

--- discord/commands/utility/link.js
+++ discord/commands/utility/link.js
@@ -1,21 +1,48 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { webApi } = require("../../config.json");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("link")
         .setDescription("Muestra los links de las salas abiertas."),
     async execute(interaction) {
-        if (
-            !global.roomsList ||
-            (global.roomsList && !global.roomsList.length)
-        ) {
-            await interaction.reply("No hay salas abiertas.");
-        } else {
-            let str = "**Salas abiertas:**\n\n--\n";
-            global.roomsList.forEach((r) => {
-                str += `- **${r.name}**\n ${r.players}/${r.maxPlayers} jugadores - ${r.link}\n--\n`;
-            });
-            await interaction.reply(str);
+        if (webApi && webApi.url && webApi.key) {
+            fetch(webApi.url + "/rooms/all", {
+                headers: {
+                    "x-api-key": webApi.key,
+                },
+            })
+                .then((res) => {
+                    if (res.ok) return res.json();
+                    else console.log(res.statusText);
+                })
+                .then(async (data) => {
+                    if (data.rooms && data.rooms.length) {
+                        const embed = new EmbedBuilder()
+                            .setColor(0xf48414)
+                            .setTitle("Salas abiertas")
+                            .addFields({ name: "\u200B", value: "\u200B" })
+                            .setTimestamp()
+                            .setFooter({
+                                text: "VIVA LA COMBA",
+                            });
+                        data.rooms.forEach((r) => {
+                            embed
+                                .addFields({
+                                    name: `${r.name}`,
+                                    value: `${r.players}/${r.maxPlayers} jugadores - ${r.link}\n`,
+                                })
+                                .addFields({ name: "\u200B", value: "\u200B" });
+                        });
+
+                        await interaction.reply({ embeds: [embed] });
+                    } else {
+                        await interaction.reply("No hay salas abiertas.");
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         }
     },
 };
--- discord/commands/utility/stats.js
+++ discord/commands/utility/stats.js
@@ -11,74 +11,78 @@ module.exports = {
                 headers: {
                     "x-api-key": webApi.key,
                 },
-            }).then(async (res) => {
-                if (res.ok) {
-                    res.json().then(async (data) => {
-                        if (data.stats && data.stats.length > 0) {
-                            console.log(data);
+            })
+                .then(async (res) => {
+                    if (res.ok) {
+                        res.json().then(async (data) => {
+                            if (data.stats && data.stats.length > 0) {
+                                console.log(data);
 
-                            data.stats.sort((a, b) => b.rating - a.rating);
+                                data.stats.sort((a, b) => b.rating - a.rating);
 
-                            let top = 15;
-                            let statsStr = "";
-                            for (
-                                let i = 1;
-                                i < top && i < data.stats.length;
-                                i++
-                            ) {
-                                let stat = data.stats[i];
-                                statsStr += `${i + 1}. **${
-                                    stat.username
-                                }:** - ${stat.rating}\n`;
+                                let top = 15;
+                                let statsStr = "";
+                                for (
+                                    let i = 1;
+                                    i < top && i < data.stats.length;
+                                    i++
+                                ) {
+                                    let stat = data.stats[i];
+                                    statsStr += `${i + 1}. **${
+                                        stat.username
+                                    }:** - ${stat.rating}\n`;
+                                }
+
+                                const embed = new EmbedBuilder()
+                                    .setColor(0xf48414)
+                                    .setTitle("Stats de PAJARITOS HAX")
+                                    .addFields(
+                                        { name: "\u200B", value: "\u200B" },
+                                        {
+                                            name: `1. ${data.stats[0].username} - ${data.stats[0].rating}`,
+                                            value: statsStr,
+                                        },
+                                        { name: "\u200B", value: "\u200B" },
+                                        {
+                                            name: "Máximo goleador",
+                                            value: `${data.maxScorer.username} (${data.maxScorer.score})`,
+                                            inline: true,
+                                        },
+                                        {
+                                            name: "Máximo asistidor",
+                                            value: `${data.maxAssister.username} (${data.maxAssister.assists})`,
+                                            inline: true,
+                                        },
+                                        {
+                                            name: "Mayor winrate",
+                                            value: `${
+                                                data.maxWinrate.username
+                                            } (${data.maxWinrate.winrate.toFixed(
+                                                3
+                                            )})`,
+                                            inline: true,
+                                        }
+                                    )
+                                    .setTimestamp()
+                                    .setFooter({
+                                        text: "VIVA LA COMBA",
+                                    });
+
+                                await interaction.reply({ embeds: [embed] });
+                            } else {
+                                await interaction.reply(
+                                    "Por algún extraño motivo no hay stats."
+                                );
                             }
-
-                            const exampleEmbed = new EmbedBuilder()
-                                .setColor(0xf48414)
-                                .setTitle("Stats de PAJARITOS HAX")
-                                .addFields(
-                                    { name: "\u200B", value: "\u200B" },
-                                    {
-                                        name: `1. ${data.stats[0].username} - ${data.stats[0].rating}`,
-                                        value: statsStr,
-                                    },
-                                    { name: "\u200B", value: "\u200B" },
-                                    {
-                                        name: "Máximo goleador",
-                                        value: `${data.maxScorer.username} (${data.maxScorer.score})`,
-                                        inline: true,
-                                    },
-                                    {
-                                        name: "Máximo asistidor",
-                                        value: `${data.maxAssister.username} (${data.maxAssister.assists})`,
-                                        inline: true,
-                                    },
-                                    {
-                                        name: "Mayor winrate",
-                                        value: `${
-                                            data.maxWinrate.username
-                                        } (${data.maxWinrate.winrate.toFixed(
-                                            3
-                                        )})`,
-                                        inline: true,
-                                    }
-                                )
-                                .setTimestamp()
-                                .setFooter({
-                                    text: "VIVA LA COMBA",
-                                });
-
-                            await interaction.reply({ embeds: [exampleEmbed] });
-                        } else {
-                            await interaction.reply(
-                                "Por algún extraño motivo no hay stats."
-                            );
-                        }
-                    });
-                } else {
-                    console.log("discord stats: " + res.statusText);
-                    await interaction.reply("Error :c");
-                }
-            });
+                        });
+                    } else {
+                        console.log("discord stats: " + res.statusText);
+                        await interaction.reply("Error :c");
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         }
     },
 };
